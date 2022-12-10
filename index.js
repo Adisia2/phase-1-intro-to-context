4ab8d8271defbf28377e5449564edd8ccc61b657
+John <+>
2022-12-10 10:12:57 +0300

    commit tree 3439bc0f1057cdcc9815b68973195a7b6cc2736a

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+function createEmployeeRecord(firstName, familyName, title,payRatePerHour){
+    
+    `firstName`
+     `familyName`
+     `title`
+     `payPerHour`
+     `timeInEvents`
+     `timeOutEvents`
+
+}
